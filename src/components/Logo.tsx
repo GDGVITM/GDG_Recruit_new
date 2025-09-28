d2e582a5c84981ddb52dc529544b25fd52a86a31
TanishqChavan10 <tan20051007@gmail.com>
2025-09-28 14:00:10 +0530

    SEO related changes & Logo replaced

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,13 +4,17 @@ interface LogoProps {
 
 export const Logo = ({ className = "" }: LogoProps) => {
   return (
-    <div className={`flex items-center space-x-3 ${className}`}>
-      <img src="/gdglogo.png" alt="GDG Logo" className="w-11 h-8" />
+    <div className={`flex items-center space-x-1 ${className}`}>
+      <img
+        src="/GDG-Sticker-Brackets.gif"
+        alt="GDG Logo"
+        className="w-15 h-12"
+      />
       <div className="flex flex-col">
         <span className="text-lg font-bold text-foreground">
           GDG Recruitment
         </span>
-        <span className="text-xs text-muted-foreground">2025-2026</span>
+        <span className="text-xs text-muted-foreground">2025-26</span>
       </div>
     </div>
   );
